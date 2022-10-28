e24759c4281c34a532629f0c6a070778d3b1e41f
Christopher Bunn <github@thenerdery.org>
2022-10-28 19:56:36 +0900

    Add downloading of assets along with changing HTML to reflect their local location.

--- choudai/choudai.py
+++ choudai/choudai.py
@@ -1,45 +1,74 @@
 from pathlib import Path
+from urllib.parse import urljoin
 
 import bs4
 import requests
 
+# def get_elements(soup: bs4.BeautifulSoup, element: str) -> bs4.element.ResultSet:
+#     return soup.find_all(element)
 
-def get_elements(soup: bs4.BeautifulSoup, element: str) -> bs4.element.ResultSet:
-    return soup.find_all(element)
+
+def download_assets(
+    session: requests.sessions.Session,
+    soup: bs4.BeautifulSoup,
+    ref_map: dict[str, str],
+    url: str,
+    path: str,
+) -> None:
+    download_path = Path(path)
+    download_path.mkdir(parents=True, exist_ok=True)
+    for tag, ref in ref_map.items():
+        elements = soup.find_all(tag)
+        for element in elements:
+            if element.has_attr(ref):
+                filename = Path(element[ref]).name
+                print(filename)
+                fileurl = urljoin(url, element[ref])
+                filepath = download_path / filename
+                element[ref] = str(download_path / filename)
+                with open(filepath, "wb") as f:
+                    f.write(session.get(fileurl).content)
 
 
 def save_html(html: str, path: str) -> None:
     save_path = Path(path)
+    print(save_path.stem)
     save_path.parent.mkdir(parents=True, exist_ok=True)
     with save_path.open("w", encoding="utf-8") as f:
         f.write(html)
 
 
-def get_soup(url: str) -> bs4.BeautifulSoup:
-    session = requests.Session()
+def get_soup(session: requests.sessions.Session, url: str) -> bs4.BeautifulSoup:
+    print(type(session))
     response = session.get(url)
     soup = bs4.BeautifulSoup(response.text, "html.parser")
-    # # print(soup.prettify("utf-8"))
-    # tags_inner = {"img": "src", "link": "href", "script": "src"}
-    # for element in html.find_all("img"):
-    #     if element.has_attr("src"):
-    #         print(url + element["src"])
     return soup
 
 
 def main():
     url = "https://www.wikipedia.org/"
-    soup = get_soup(url=url)
+    # map the elements we want to download byt tag with their reference identifiers
+    ref_map = {"img": "src", "link": "href", "script": "src"}
+
+    session = requests.Session()
+    soup = get_soup(session=session, url=url)
+
     # print(soup)
     print(type(soup))
-    images = get_elements(soup=soup, element="img")
-    print(type(images[0]))
-    num_images = len(images)
-    print(num_images)
-    p = Path(".")
-    print(type(p.resolve()))
+    # images = get_elements(soup=soup, element="img")
+    # print(images)
+    # print(type(images[0]))
+    # num_images = len(images)
+    # print(num_images)
+    download_assets(
+        session=session,
+        soup=soup,
+        ref_map=ref_map,
+        url=url,
+        path="www.wikipedia.org_files",
+    )
     # save to file
-    save_html(html=str(soup), path="tmp.html")
+    save_html(html=str(soup), path="www.wikipedia.org.html")
 
 
 if __name__ == "__main__":
